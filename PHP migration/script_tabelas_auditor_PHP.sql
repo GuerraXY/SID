af9f19db4f1c133b9fff216b59c08b4beb3fc579
mamoe-iscteiul <55883178+mamoe-iscteiul@users.noreply.github.com>
2020-04-02 14:15:23 +0100

    conversão para varchars
    converti os datatypes para varchar para facilitar migração

--- PHP migration/script_tabelas_auditor_PHP.sql
+++ PHP migration/script_tabelas_auditor_PHP.sql
@@ -6,7 +6,7 @@ CREATE TABLE if not exists log_Utilizador (
 	id int PRIMARY KEY AUTO_INCREMENT,
 	QuemAlterou varchar(100),
 	TipoOperacao varchar(6),
-	DataAlteracao timestamp,
+	DataAlteracao varchar(20),
 	EmailAntigo varchar(100),
 	EmailNovo varchar(100),
 	NomeAntigo varchar(200),
@@ -18,25 +18,25 @@ CREATE TABLE if not exists log_Utilizador (
 CREATE TABLE if not exists log_InsertRondaExtra (
 	Id int PRIMARY KEY AUTO_INCREMENT,
 	QuemInseriu varchar(100),
-	DataInsercao timestamp,
+	DataInsercao varchar(8),
 	EmailNovo varchar(100), 
-	DataHoraInicioNova timestamp null default null,
-	DataHoraFimNova timestamp null default null
+	DataHoraInicioNova varchar(20),
+	DataHoraFimNova varchar(20)
 	);
 	
 CREATE TABLE if not exists log_RondaPlaneada (
 	id int PRIMARY KEY AUTO_INCREMENT,
 	QuemAlterou varchar(100),
 	TipoOperacao varchar(6),
-	DataAlteracao TIMESTAMP,
+	DataAlteracao varchar(8),
 	EmailAntigo varchar(100),
 	EmailNovo varchar(100),
 	DiaSemanaAntigo varchar(20),
 	DiaSemanaNovo varchar(20),
-	HoraRondaInicioAntiga time,
-	HoraRondaInicioNova time,
-    HoraRondaFimAntiga time,
-	HoraRondaFimNova time
+	HoraRondaInicioAntiga varchar(8),
+	HoraRondaInicioNova varchar(8),
+    HoraRondaFimAntiga varchar(8),
+	HoraRondaFimNova varchar(8)
 	);
 
 CREATE TABLE if not exists log_DiaSemana (
@@ -45,28 +45,28 @@ CREATE TABLE if not exists log_DiaSemana (
 	TipoOperacao varchar(6),
 	DiaSemanaAntigo varchar(20),
 	DiaSemanaNovo varchar(20),
-	HoraRondaInicioAntiga time,
-	HoraRondaInicioNova time,
-    HoraRondaFimAntiga time,
-	HoraRondaFimNova time
+	HoraRondaInicioAntiga varchar(8),
+	HoraRondaInicioNova varchar(8),
+    HoraRondaFimAntiga varchar(8),
+	HoraRondaFimNova varchar(8)
 	);
 
 CREATE TABLE if not exists log_UpdateSistema (
 	id int PRIMARY KEY AUTO_INCREMENT,
 	QuemAlterou varchar(100),
-	DataAlteracao timestamp,
-	LimiteTemperaturaAntigo decimal(6,2),
-	LimiteTemperaturaNovo decimal(6,2),
-	LimiteHumidadeAntigo decimal(6,2),
-	LimiteHumidadeNovo decimal(6,2),
-	LimiteLuminosidadeAntigo decimal(6,2),
-	LimiteLuminosidadeNovo decimal(6,2)
+	DataAlteracao varchar(8),
+	LimiteTemperaturaAntigo varchar(6),
+	LimiteTemperaturaNovo varchar(6),
+	LimiteHumidadeAntigo varchar(6),
+	LimiteHumidadeNovo varchar(6),
+	LimiteLuminosidadeAntigo varchar(6),
+	LimiteLuminosidadeNovo varchar(6)
 	);
     
 CREATE TABLE if not exists log_Select (
 		id int PRIMARY KEY AUTO_INCREMENT,
 		QuemViu varchar(100),
-        DataVisualizacao timestamp,
+        DataVisualizacao varchar(20),
         TabelaVista varchar(20),
         atributosVistos text
 		);
